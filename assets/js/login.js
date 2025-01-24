92fb1f33c62329808b639d751002be7622af206e
Hande Eylül Memikoğlu <handememikoglu@gmail.com>
2025-01-24 16:00:06 +0300

    input values were checked in the JSON file.

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,24 +2,34 @@ const mail = document.querySelector(".login-email");
 const password = document.querySelector(".login-password");
 const button = document.querySelector(".login-button");
 
-fetch("assets/json/users.json")
-.then(res => {
-    if(res.ok){
-        return res.json();
-    }
-}).then(data => {
-    
-
-}).catch(e => {
-    console.log("bir hata oldu.");
-    
-})
-
 button.addEventListener("click", (event) => {
     event.preventDefault();
     const userMail = mail.value;
     const userPassword = password.value;
     console.log(userMail);
     console.log(userPassword);
+    fetch("./assets/json/users.json")
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        }
+    }).then(data => {
+        const users = data.find(user => user.email === userMail && user.password == userPassword);
+        
+        if(users){
+            console.log("kullanıcı bulundu");
+            
+        }else {
+            console.log("kullanıcı bulunamadı");
+        }
+        console.log(userMail);
+        console.log(userPassword);
+
+    }).catch(e => {
+        console.log("bir hata oldu.");
+        
+    })
+
+
     
 });
